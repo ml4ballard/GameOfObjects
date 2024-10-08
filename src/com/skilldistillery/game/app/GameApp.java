72754390e76ff9e6a3113ef6dc7ef527255e5c6b
Maureen Ballard <moballard@comcast.net>
2024-10-07 21:27:15 -0600

    initial setup

---
+++ src/com/skilldistillery/game/app/GameApp.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.game.app;
+
+public class GameApp{
+	public static void main(String[] args){
+		GameApp game = new GameApp();
+		game.run();
+	}
+
+	public void run() {
+	
+	}
+}
---
+++ src/com/skilldistillery/game/entities/Character.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.game.entities;
+
+	public abstract class Character {
+		private String name;
+}
