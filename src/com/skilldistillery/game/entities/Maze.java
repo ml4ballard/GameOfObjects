33a5261a62e5c64d76cdd0eaefca9e17088b7a21
Maureen Ballard <moballard@comcast.net>
2024-10-11 09:49:02 -0600

    monster classes 5

--- src/com/skilldistillery/game/entities/Maze.java
+++ src/com/skilldistillery/game/entities/Maze.java
@@ -2,16 +2,17 @@ package com.skilldistillery.game.entities;
 
 public class Maze {
 	private String[][] themaze = new String[6][4]; //6 is floor row, 4 is left or right column
+	
 	public void initializeMaze() {
 
 		// Fill the array with values to symbolize the Maze
 
-		themaze[0] = new String[] { "enter", "wall", "wall", "wall" };
-		themaze[1] = new String[] { "walk", "getWeapon", "wall", "wall" };
-		themaze[2] = new String[] { "wall", "walk", "weakMonster", "wall" };
-		themaze[3] = new String[] { "wall", "wall", "walk", "wall" };
-		themaze[4] = new String[] { "wall", "wall", "strongMonster", "wall" };
-		themaze[5] = new String[] { "wall", "wall", "exitAndLive", "wall" };
+		themaze[0] = new String[] { " ", "w", "w", "w" };
+		themaze[1] = new String[] { " ", "weapon", "w", "w" };
+		themaze[2] = new String[] { "w", " ", "weakMonster", "w" };
+		themaze[3] = new String[] { "w", "w", " ", "w" };
+		themaze[4] = new String[] { "w", "w", "strongMonster", "w" };
+		themaze[5] = new String[] { "w", "w", "exitAndLive", "w" };
 
 /*		// Print the array for fun
 		for (int i = 0; i < themaze.length; i++) {
