667ea36c3ed0d78cee4891d44b9796245485830d
yessenia <yesseniaperez.n5@gmail.com>
2024-10-10 19:35:51 -0600

    characters

--- src/com/skilldistillery/game/app/GameApp.java
+++ src/com/skilldistillery/game/app/GameApp.java
@@ -2,8 +2,8 @@ package com.skilldistillery.game.app;
 
 import java.util.Scanner;
 
-public class GameApp{
-	public static void main(String[] args){
+public class GameApp {
+	public static void main(String[] args) {
 		GameApp game = new GameApp();
 		game.run();
 	}
@@ -12,20 +12,21 @@ public class GameApp{
 		GameApp welcome = new GameApp();
 		welcome.welcomeMenu();
 	}
-	public void welcomeMenu() { 
+
+	public void welcomeMenu() {
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Welcome to the Halloween Maze!\n" 
-		+ "Would you like to play and move through the maze? Yes or No?\n");
+		System.out.println(
+				"Welcome to the Halloween Maze!\n" + "Would you like to play and move through the maze? Yes or No?\n");
 		keyboard.nextLine();
 		System.out.println("Please pick your Hero");
-		
+
 		System.out.println("---MENU-------");
 		System.out.println("1) King       ");
 		System.out.println("2) Queen      ");
 		System.out.println("3) Clown      ");
 		System.out.println("--------------");
-		
-		int selection;
+
+		int selection = 0;
 		if (keyboard.hasNextInt()) {
 			selection = keyboard.nextInt();
 			keyboard.nextLine();
---
+++ src/com/skilldistillery/game/entities/Clown.java
@@ -0,0 +1,34 @@
+package com.skilldistillery.game.entities;
+
+public class Clown extends Character {
+	private String name = "I'm the Clown";
+	private String noise ;
+	private int points = 10;
+
+	
+	public Clown() {
+		super();
+	}
+	
+	public Clown(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void makeNoise() {
+		super.makeNoise();
+		System.out.println("Prepare for a surprise! Laughter is the best weapon!");
+	}
+	@Override
+	public void health(int points) {
+		System.out.println("You have" + points);
+		super.health(points);
+	}
+
+	@Override
+	public String toString() {
+		return "Clown [name=" + name + ", noise=" + noise + ", points=" + points + "]";
+	}
+	
+
+}
